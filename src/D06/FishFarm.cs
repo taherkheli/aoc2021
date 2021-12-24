1e215aa4e2a8ee86ff7924faee0256f8db8aefaf
Bilal Taherkheli <bilal76@gmail.com>
2021-12-24 18:41:49 +0100

    Day6 partI done

---
+++ src/D06/FishFarm.cs
@@ -0,0 +1,39 @@
+﻿namespace aoc.D06
+{
+  public class FishFarm
+  {
+    private List<int> _intList;
+
+    public FishFarm(int[] ages)
+    {
+      _intList = new List<int>();
+
+      for (int i = 0; i < ages.Length; i++)
+        _intList.Add(ages[i]);      
+    }
+
+    public long Simulate(int days)
+    {
+      for (int i = 0; i < days; i++)
+      {
+        var newBorns = 0;
+
+        for (int j = 0; j < _intList.Count; j++)
+        {
+          if (_intList[j] == 0)
+          {
+            _intList[j] = 6;
+            newBorns++;
+          }
+          else
+            _intList[j]--;
+        }        
+
+        for (int k = 0; k < newBorns; k++)
+          _intList.Add(8);
+      }
+
+      return _intList.Count;
+    }
+  }
+}
---
+++ test/D06/TC01.cs
@@ -0,0 +1,57 @@
+﻿using aoc.D06;
+using Xunit;
+
+namespace aoc.test.D06
+{
+  public class TC01
+  {
+    [Fact]
+    public void InitialI()
+    {
+      int expected = 5934;
+      var ages = new int[] { 3, 4, 3, 1, 2 };
+
+      var farm = new FishFarm(ages);
+      var actual = farm.Simulate(80);
+
+      Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void PartI()
+    {
+      int expected = 371379;
+      var ages = Parser.Parse(@"./D06/input.txt");
+            
+      var farm = new FishFarm(ages);
+      var actual = farm.Simulate(80);
+
+      Assert.Equal(expected, actual);
+    }
+
+  //  [Fact]
+  //  public void InitialII()
+  //  {
+  //    long expected = 26984457539;
+  //    var ages = new int[] { 3, 4, 3, 1, 2 };
+
+  //    var farm = new FishFarm(ages);
+  //    var actual = farm.Simulate(256);
+
+  //    Assert.Equal(expected, actual);
+  //  }
+
+  //  [Fact]
+  //  public void PartII()
+  //  {
+  //    long expected = 26984457539;
+  //    var ages = Parser.Parse(@"./D06/input.txt");
+
+  //    var farm = new FishFarm(ages);
+  //    var actual = farm.Simulate(256);
+
+  //    Assert.Equal(expected, actual);
+  //  }
+
+  }
+}
